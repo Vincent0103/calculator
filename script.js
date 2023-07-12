001f484f3641f38d00c269dd90c5d176a8741f09
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-07-12 23:14:30 +0200

    Make calculator functional even with big expression, -

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 function add(a, b) {
-    return (a + b !== NaN) ? a + b : "err";
+    return (a + b !== NaN) ? (Math.round((a + b) * 10**5) / 10**5) : "err";
 }
 
 function subtract(a, b) {
-    return (a - b !== NaN) ? a - b : "err";
+    return (a - b !== NaN) ? (Math.round((a - b) * 10**5) / 10**5) : "err";
 }
 
 function multiply(a, b) {
-    return (a * b !== NaN) ? a * b : "err";
+    return (a * b !== NaN) ? (Math.round((a * b) * 10**5) / 10**5) : "err";
 }
 
 function divide(a, b) {
-    return (a / b !== NaN) ? a / b : "err";
+    if (a / b === NaN) {
+        return "err";
+    } else if (a / b === Infinity) {
+        return "Cannot divide by 0 idiot";
+    } else {
+        return Math.round((a / b) * 10**5) / 10**5;
+    }
 }
 
 function operate(a, op, b) {
-    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    if (a % 1 != 0) {
+        a = parseFloat(a);
+    } else if (!Number.isInteger(a)) {
         a = parseInt(a);
+    }
+
+    if (b % 1 != 0) {
+        b = parseFloat(b);
+    } else if (!Number.isInteger(b)) {
         b = parseInt(b);
     }
 
@@ -38,41 +51,53 @@ function operate(a, op, b) {
     }
 }
 
-function getOperator(obj) {
+function getOperators(arr) {
     // check if the obj parameter is a string or an array
-    if (typeof obj === "string") return obj.match(/[+\-*/]/g);
-    return obj.filter(item => item.match(/[+\-*/]/g));
+    if (typeof arr === "string") return arr.match(/[+\-*/]/g);
+    return arr.filter(item => item.match(/[+\-*/]/g));
 }
 
 function calcDisplay(btn) {
-    console.log("doing");
+    let result = 0;
     const screenNums = document.querySelector(".screen-nums");
 
     if (Number.isInteger(parseInt(btn.textContent))) {
         screenNums.textContent += btn.textContent;
 
     // check if the clicked btn is an operator
-    } else if (getOperator(btn.textContent) !== null && btn.textContent === getOperator(btn.textContent)[0]) {
+    } else if (getOperators(btn.textContent) !== null && btn.textContent === getOperators(btn.textContent)[0]) {
         screenNums.textContent += ` ${btn.textContent} `;
+        operation = screenNums.textContent.split(" ");
+        console.log(operation);
+
+        /* when we split the operation, the last element is an empty string due
+         to the extra spaces around the operator in screenNums */
+        if (operation.length === 5) {
+            result += operate(operation[0], operation[1], operation[2]);
+            screenNums.textContent = `${result} ${operation[3]} `;
+        }
 
     }
     else if (btn.textContent === "enter") {
-        const operations = screenNums.textContent.split(" ");
-        let result = 0;
-        operations.forEach((operation, i) => {
-            if (operation === "+" || operation === "-" || operation === "*" || operation === "/") {
-                result += operate(operations[i - 1], operations[i], operations[i + 1]);
-            }
-        })
+        let operation = screenNums.textContent.split(" ");
+        console.log(operation)
+        if (operation.length === 1) {
+            result = screenNums.textContent;
+        } else if (operation[2] === "") {
+            result = "";
+        } else {
+            result += operate(operation[0], operation[1], operation[2]);
+        }
         screenNums.textContent = result;
 
-    } else if (btn.textContent === "clear") screenNums.textContent = "";
+    } else if (btn.textContent === "AC") screenNums.textContent = "";
 
 }
 
 window.addEventListener("DOMContentLoaded", () => {
     const calcDigits = document.querySelectorAll(".calc-digits");
-    const taskBtns = document.querySelectorAll(".task-btns");
+    const enterBtn = document.querySelector(".enter-btn");
+    const clrBtns = document.querySelectorAll(".clr-btns");
 
     calcDigits.forEach(digit => {
         digit.addEventListener("click", () => {
@@ -80,7 +105,11 @@ window.addEventListener("DOMContentLoaded", () => {
         })
     })
 
-    taskBtns.forEach(btn => {
+    enterBtn.addEventListener("click", () => {
+        calcDisplay(enterBtn);
+    })
+
+    clrBtns.forEach(btn => {
         btn.addEventListener("click", () => {
             calcDisplay(btn);
         })
